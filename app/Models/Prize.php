80a1cafc7a05ce21000a297ed4e665f744550195
kaungsattkhant <ookaung42@gmail.com>
2025-04-25 15:17:58 +0630

    done

--- app/Models/Prize.php
+++ app/Models/Prize.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Prize extends Model
 {
     use HasFactory;
-    protected $fillable=['name','prize'];
+    protected $fillable=['name','prize','game_setting_id'];
     public function prizes_images(){
         return $this->hasMany(PrizeItemImage::class);
     }
