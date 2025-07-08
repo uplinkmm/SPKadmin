c61ac851bb685eb9aaee0d52baf68fc5fc9994d1
NayPaingSoe <naypaingsoe042@gmail.com>
2025-07-08 11:44:35 +0630

    fix: marquee create

--- resources/js/Components/Ads/Ads.vue
+++ resources/js/Components/Ads/Ads.vue
@@ -33,7 +33,9 @@
             <div class="overflow-x-auto">
                 <div class="overflow-hidden">
                     <SelectionPaginationCount
-                        :handleChange="(value) => (per_page=value, getAds(true))"
+                        :handleChange="
+                            (value) => ((per_page = value), getAds(true))
+                        "
                         :initialValue="per_page"
                     />
                     <div class="table-container">
@@ -55,7 +57,10 @@
                                     class="border-b border-l border-neutral-200"
                                 >
                                     <td class="whitespace-nowrap">
-                                        {{ ++index + (currentPage - 1) * per_page }}
+                                        {{
+                                            ++index +
+                                            (currentPage - 1) * per_page
+                                        }}
                                     </td>
                                     <td class="whitespace-nowrap">
                                         <div>
@@ -96,7 +101,10 @@
                                             <i class="fal fa-edit"></i>
                                         </button>
                                         <button
-                                            @click="delete_ads.id = ads.id;delete_ads.type = ads.type"
+                                            @click="
+                                                delete_ads.id = ads.id;
+                                                delete_ads.type = ads.type;
+                                            "
                                             data-twe-toggle="modal"
                                             data-twe-target="#ads_delete_confirm"
                                             data-twe-ripple-init
@@ -190,8 +198,13 @@
                             v-model="type"
                             class="block w-full py-2 px-2 border border-gray-400 text-sm rounded-md bg-white focus:ring-0 focus:shadow-none relative"
                         >
-                            <option value="ads">Ads</option>
-                            <option value="promotion">Promotion</option>
+                            <option
+                                v-for="t in types"
+                                :value="t.value"
+                                :key="t"
+                            >
+                                {{ t.label }}
+                            </option>
                         </select>
                     </div>
                     <div class="mb-6">
@@ -216,7 +229,7 @@
                             class="block w-full py-2 px-2 border border-gray-400 text-sm rounded-md bg-white focus:ring-0 focus:shadow-none"
                         />
                     </div>
-                    <div class="mb-6">
+                    <div v-if="type != 'marquee'" class="mb-6">
                         <label for="" class="text-sm mb-3 relative block"
                             >Photo</label
                         >
@@ -264,95 +277,98 @@
             </div>
         </div>
     </div>
-      <!-- Ads Delete Confirm Modal -->
-      <div
-            data-twe-modal-init
-            class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
-            id="ads_delete_confirm"
-            tabindex="-1"
-            aria-labelledby="ModalLabel"
-            aria-hidden="true"
+    <!-- Ads Delete Confirm Modal -->
+    <div
+        data-twe-modal-init
+        class="fixed left-0 top-0 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+        id="ads_delete_confirm"
+        tabindex="-1"
+        aria-labelledby="ModalLabel"
+        aria-hidden="true"
+    >
+        <div
+            data-twe-modal-dialog-ref
+            class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]"
         >
             <div
-                data-twe-modal-dialog-ref
-                class="pointer-events-none relative w-auto translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]"
+                class="pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-4 outline-none"
             >
                 <div
-                    class="pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-4 outline-none"
+                    class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 p-4"
                 >
-                    <div
-                        class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 p-4"
+                    <h5
+                        class="text-xl font-medium leading-normal text-surface"
+                        id="ModalLabel"
+                    ></h5>
+                    <button
+                        type="button"
+                        id="closeModal"
+                        class="box-content rounded-none border-none text-neutral-500 hover:text-neutral-800 hover:no-underline focus:text-neutral-800 focus:opacity-100 focus:shadow-none focus:outline-none"
+                        data-twe-modal-dismiss
+                        aria-label="Close"
                     >
-                        <h5
-                            class="text-xl font-medium leading-normal text-surface"
-                            id="ModalLabel"
-                        >
-                          
-                        </h5>
-                        <button
-                            type="button"
-                            id="closeModal"
-                            class="box-content rounded-none border-none text-neutral-500 hover:text-neutral-800 hover:no-underline focus:text-neutral-800 focus:opacity-100 focus:shadow-none focus:outline-none"
-                            data-twe-modal-dismiss
-                            aria-label="Close"
-                        >
-                            <span class="[&>svg]:h-6 [&>svg]:w-6">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="currentColor"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </span>
-                        </button>
+                        <span class="[&>svg]:h-6 [&>svg]:w-6">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                                stroke-width="1.5"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </span>
+                    </button>
+                </div>
+                <div
+                    class="relative flex-auto px-4 py-2"
+                    data-twe-modal-body-ref
+                >
+                    <div class="">
+                        <p class="text-lg font-bold relative block">
+                            Are you sure you want to delete this
+                            {{ delete_ads.type }}?
+                        </p>
                     </div>
-                    <div
-                        class="relative flex-auto px-4 py-2"
-                        data-twe-modal-body-ref
+                </div>
+                <div
+                    class="flex flex-shrink-0 flex-wrap items-center justify-end border-t-2 border-neutral-100 p-4 gap-x-4"
+                >
+                    <button
+                        type="button"
+                        class="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs text-black focus:outline-none focus:ring-00"
+                        data-twe-modal-dismiss
+                        data-twe-ripple-init
+                        id="modalCloseConfirm"
+                        data-twe-ripple-color="light"
                     >
-                        <div class="">
-                            <p
-                                class="text-lg font-bold relative block"
-                            >Are you sure you want to delete this {{ delete_ads.type }}?</p>
-                        </div>
-                    </div>
-                    <div
-                        class="flex flex-shrink-0 flex-wrap items-center justify-end border-t-2 border-neutral-100 p-4 gap-x-4"
+                        Close
+                    </button>
+                    <button
+                        type="button"
+                        @click="deleteAds"
+                        class="rounded bg-red-600 px-8 pb-2 pt-2.5 text-xs text-white hover:bg-red-700 focus:outline-none focus:ring-0 active:bg-red-800"
                     >
-                        <button
-                            type="button"
-                            class="inline-block rounded bg-primary-100 px-6 pb-2 pt-2.5 text-xs text-black focus:outline-none focus:ring-00"
-                            data-twe-modal-dismiss
-                            data-twe-ripple-init
-                            id="modalCloseConfirm"
-                            data-twe-ripple-color="light"
-                        >
-                            Close
-                        </button>
-                        <button
-                            type="button"
-                            @click="deleteAds"
-                            class="rounded bg-red-600 px-8 pb-2 pt-2.5 text-xs text-white hover:bg-red-700 focus:outline-none focus:ring-0 active:bg-red-800"
-                        >
-                            Delete
-                        </button>
-                    </div>
+                        Delete
+                    </button>
                 </div>
             </div>
         </div>
+    </div>
 </template>
 
 <script>
 import { initTWE, Modal, Ripple, Dropdown } from "tw-elements";
 import { mapGetters, mapMutations } from "vuex";
-import { getApiData, postApiData,deleteApiData } from "../../utilities/ajax-helpers";
+import {
+    getApiData,
+    postApiData,
+    deleteApiData,
+} from "../../utilities/ajax-helpers";
 import SearchBox from "../Common/SearchBox.vue";
 import ImgPreview from "./imgPreview.vue";
 import WebPagination from "../Common/webPagination.vue";
@@ -378,10 +394,24 @@ export default {
             icon_preview: "",
             per_page: 50,
             type: "ads",
-            delete_ads:{
-                id:null,
-                type:"ads"
-            }
+            types: [
+                {
+                    value: "ads",
+                    label: "Ads",
+                },
+                {
+                    value: "promotion",
+                    label: "Promotion",
+                },
+                {
+                    value: "marquee",
+                    label: "Marquee",
+                },
+            ],
+            delete_ads: {
+                id: null,
+                type: "ads",
+            },
         };
     },
     computed: {
