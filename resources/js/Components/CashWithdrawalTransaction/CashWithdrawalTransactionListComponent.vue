e457f445cd519745c3092ce3ff426c4eed33d132
NayPaingSoe <naypaingsoe042@gmail.com>
2025-07-09 14:23:36 +0630

    fix: add type

--- resources/js/Components/CashWithdrawalTransaction/CashWithdrawalTransactionListComponent.vue
+++ resources/js/Components/CashWithdrawalTransaction/CashWithdrawalTransactionListComponent.vue
@@ -27,7 +27,11 @@
         <div class="flex flex-col bg-white px-4 pt-4 pb-12 rounded-md">
             <div class="">
                 <SelectionPaginationCount
-                    :handleChange="(value) => (per_page=value, getTransactionList(true))"
+                    :handleChange="
+                        (value) => (
+                            (per_page = value), getTransactionList(true)
+                        )
+                    "
                     :initialValue="per_page"
                 />
                 <div class="">
@@ -38,6 +42,7 @@
                                     <th scope="col">No</th>
                                     <th scope="col">Name</th>
                                     <th scope="col">Phone Number</th>
+                                    <th scope="col">Acc Name</th>
                                     <th scope="col">Type</th>
                                     <th scope="col">Transfer Phone Number</th>
                                     <th scope="col">Amount</th>
@@ -62,13 +67,21 @@
                                         }}
                                     </td>
                                     <td class="whitespace-nowrap">
-                                        <a class="underline text-blue-500 font-semibold" :href="`balance?user_id=${transaction.customer.id}`">  {{ transaction.customer.name }}</a>
+                                        <a
+                                            class="underline text-blue-500 font-semibold"
+                                            :href="`balance?user_id=${transaction.customer.id}`"
+                                        >
+                                            {{ transaction.customer.name }}</a
+                                        >
                                     </td>
                                     <td class="whitespace-nowrap">
                                         {{ transaction.customer.phone_number }}
                                     </td>
                                     <td class="whitespace-nowrap">
                                         {{ transaction.account_name }}
+                                    </td>
+                                    <td class="whitespace-nowrap">
+                                        {{ transaction.payment_provider }}
                                     </td>
                                     <td class="whitespace-nowrap">
                                         {{ transaction.phone_number }}
@@ -212,9 +225,7 @@
                                         Complete Deposit:
                                     </td>
                                     <td colspan="5">
-                                        {{
-                                            total_completed_withdrawal
-                                        }}
+                                        {{ total_completed_withdrawal }}
                                     </td>
                                 </tr>
                                 <tr
