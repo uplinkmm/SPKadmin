ace454a1fdaf29cc1e46f8bc9f6248d245aab6af
kaungsattkhant <ookaung42@gmail.com>
2025-09-15 16:23:10 +0630

    lottery added

---
+++ app/Http/Requests/Admin/LotteryPromotionRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+
+class LotteryPromotionRequest extends FormRequest
+{
+    public function rules()
+    {
+        $id=$this->get('id');
+        return [
+            'name'=>'required',
+            'phone_number' => [
+                'required',
+                $id ? null : Rule::unique('contacts'),
+            ],
+        ];
+    }
+    public function authorize()
+    {
+        return parent::authorize();
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        parent::failedValidation($validator);
+    }
+}
