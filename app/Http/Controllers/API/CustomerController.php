c18a4b70bfbcaeae338d5c44ad20b0c2dbbdfe8b
kaungsattkhant <ookaung42@gmail.com>
2026-03-31 11:30:09 +0630

    merge:2d setting

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -19,7 +19,6 @@ class CustomerController extends Controller
 
     public function store(CustomerStoreRequest $request)
     {
-       
         $data = $this->customerRepo->store($request);
         ResponseData($data);
     }
